c64aa706a12ac3abb28f164cf1ff419ac11dabe0
Evelin Raiane <evelin_raiane@hotmail.com>
2022-09-02 12:00:09 -0300

    adiciona clientes

---
+++ ProjetoVendaVueNETCore.App/src/modules/cliente/views/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import Titulo from "../../shared/views/titulo";
+import { mapActions } from "vuex";
+
+export default {
+  name: "Clientes",
+  components: {
+    Titulo,
+  },
+  data() {
+    return {
+      titulo: "Clientes",
+      clientes: [],
+    };
+  },
+
+  methods: {
+    /* eslint-disable */
+    ...mapActions({
+      listarClientes: "clientes/listarClientes",
+      salvar: "clientes/salvar",
+      remover: "clientes/remover",
+      atualizar: "clientes/atualizar",
+    }),
+  },
+
+  mounted() {
+  },
+};
+</script>
